fced290cb2821a3769c5b1ef027fa127b49ad47d
Vinson Chuong <vinsonchuong@gmail.com>
2015-06-20 10:22:23 -0700

    Deal with failure on TravisCI

--- src/bin/clone-link-dependencies.js
+++ src/bin/clone-link-dependencies.js
@@ -1,29 +1,34 @@
-import {exec} from 'node-promise-es6/child-process';
-import fs from 'node-promise-es6/fs';
+#!/usr/bin/env node
+var execSync = require('child_process').execSync;
+var path = require('path');
 
-async function run() {
-  const {linkDependencies = {}} = await fs.readJson('package.json');
-  for (const dependencyName of Object.keys(linkDependencies)) {
-    const dependencyPath = linkDependencies[dependencyName];
-    process.stdout.write(
-      `Cloning 'vinsonchuong/${dependencyName}' to '${dependencyPath}'...`);
-    try {
-      await exec(
-        `git clone 'https://github.com/vinsonchuong/${dependencyName}' '${dependencyPath}'`);
-      process.stdout.write('done');
-    } catch (e) {
-      if (e.message.match(/destination path .*? already exists/)) {
-        process.stdout.write('already cloned');
-      } else {
-        throw e;
-      }
-    } finally {
-      process.stdout.write('\n');
+var linkDependencies = Object(
+  require(path.resolve('package.json')).linkDependencies);
+Object.keys(linkDependencies).forEach(function(dependencyName) {
+  var dependencyPath = linkDependencies[dependencyName];
+  process.stdout.write([
+    'Cloning "vinsonchuong/',
+    dependencyName,
+    '" to "',
+    dependencyPath,
+    '"...'
+  ].join(''));
+  try {
+    execSync([
+      'git clone \'https://github.com/vinsonchuong/',
+      dependencyName,
+      '\' \'',
+      dependencyPath,
+      '\''
+    ].join(''), {stdio: ['pipe', 'pipe', 'pipe']});
+    process.stdout.write('done');
+  } catch (e) {
+    if (e.message.match(/destination path .*? already exists/)) {
+      process.stdout.write('already cloned');
+    } else {
+      throw e;
     }
+  } finally {
+    process.stdout.write('\n');
   }
-}
-
-run().catch(e => {
-  process.stderr.write(`${e.stack}\n`);
-  process.exit(1);
 });
