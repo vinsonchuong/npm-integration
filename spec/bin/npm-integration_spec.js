83081fc85fbf6a4441bb71b71a7c94a970679c6d
Vinson Chuong <vinsonchuong@gmail.com>
2015-06-20 18:44:46 -0700

    Remove unused skeleton files

--- spec/bin/npm-integration_spec.js
+++
@@ -1,8 +0,0 @@
-import {exec} from 'node-promise-es6/child-process';
-
-describe('npm-integration', function() {
-  it('outputs "3...2...1...Hello World!"', async function() {
-    const {stdout} = await exec('npm-integration');
-    expect(stdout.trim()).toBe('3...2...1...Hello World!');
-  });
-});
--- spec/index_spec.js
+++
@@ -1,7 +0,0 @@
-import npmIntegration from 'npm-integration';
-
-describe('npm-integration', function() {
-  it('exports "Hello World!"', function() {
-    expect(npmIntegration).toBe('Hello World!');
-  });
-});
